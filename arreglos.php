a5405f365a34306552f55c982b1995aac25f8681
Carlos Santos <carlosalbertosantos@uts.edu.co>
2023-12-13 23:24:31 -0500

    First practice with array

---
+++ arreglos.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$edades = [20, 10, 15]; // Colocamos los [] para indicar que es un arreglo
+
+echo "La primera edad que esta dentro de el arreglo es: ".$edades[0]."\n";
+
+// Otra forma para declarar un arreglo
+
+$inventario = array("Papas", "Coca Cola", "Pepsi", "Cerveza"); // el metodo array() es para declarar un arreglo
+
+echo "El primer elemento del arreglo inventario es: ".$inventario[0]."\n";
+
+?>

ee69f9c2ea793f0b6685f593e0d341188d71e168
Carlos Santos <carlosalbertosantos@uts.edu.co>
2023-12-22 21:43:16 -0500

    Add class while

---
+++ while.php
@@ -0,0 +1,17 @@
+<?php
+    //Ciclo while
+
+    //es un blucle, es una estructura de control nos
+    //ayuda a repetir un bloque de codigo las veces
+    //que lo necesitemos.
+
+    $a = 0; // inicializamos la variable
+
+    while ($a <= 10) {//La condicion que espera el while como parametro si es true se repetira las veces que sean necesarias hasta que se transforme en false esa condicion ahi va a parar
+        echo "Iteracion : $a";
+        $a++; //recordando que el while ira repitiendo un bloque de codigo hacemos que por cada iteracion contador aumente a 1 asi podemos llegar a 10
+    } echo "\n";
+
+
+
+?>

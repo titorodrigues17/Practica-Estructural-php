e07667b2cf996698a1b2a6c8316f89748625d69e
Carlos Santos <carlosalbertosantos@uts.edu.co>
2023-12-28 22:42:53 -0500

    Add funciones class

---
+++ funciones.php
@@ -0,0 +1,36 @@
+<?php
+/*
+    En PHP, una función es un bloque de código que se puede llamar desde cualquier parte de un programa para realizar una tarea específica. Las funciones permiten organizar y reutilizar el código, lo que facilita el desarrollo y el mantenimiento de aplicaciones web
+*/
+
+
+function get_pokemon() {
+
+    $numero_aleatorio = rand(1 , 5);
+
+    switch ($numero_aleatorio) {
+        case 1:
+            echo "Pikachu";
+            break;
+
+        case 2:
+            echo "Charmander";
+            break;
+        
+        case 3:
+            echo "Bulbasaur";
+            break;
+
+        case 4:
+            echo "Squirtle";
+            break;
+
+        default:
+            echo "El caracter que acaba de ingresar no es correcto";
+            break;
+    }
+} echo "\n";
+
+get_pokemon();
+
+?>
